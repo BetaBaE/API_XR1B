272aebb02a901ab859e2b6051eeb3a51c660f5bc
youssefihrai <49070572+youssefihrai@users.noreply.github.com>
2023-11-21 10:41:07 +0100

    modification des virements

--- database/querys.js
+++ database/querys.js
@@ -186,7 +186,7 @@ exports.ordervirements = {
     and Etat = 'En cours'
     and [orderVirementId] = @ovId`,
   updateVirements: `update [dbo].[DAF_Order_virements] set Etat = 'Reglee'
-                      where id = @id`,
+                      where id = @id and Etat=<>'Annuler'`,
 
   updateLogFacture: `update [dbo].[DAF_LOG_FACTURE] set Etat = 'Reglee'
                         where ModePaiementID = @id`,
