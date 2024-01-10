950e22845d693341c041629e5e39d074c4362cdc
youssefihrai <49070572+youssefihrai@users.noreply.github.com>
2024-01-10 11:07:04 +0100

    Séparation des désignation

--- controllers/designationFacture.js
+++ controllers/designationFacture.js
@@ -1,7 +1,6 @@
 const { getConnection, getSql } = require("../database/connection");
 const { designation } = require("../database/querys");
-
-exports.getdesignations = async(req, res) => {
+exports.getdesignations = async (req, res) => {
     try {
         let range = req.query.range || "[0,9]";
         let sort = req.query.sort || '["iddesignation" , "ASC"]';
@@ -20,17 +19,41 @@ exports.getdesignations = async(req, res) => {
 
         const result = await pool.request().query(
             `${designation.getDesignation} ${queryFilter} Order by ${sort[0]} ${
-          sort[1]
-        }
-        OFFSET ${range[0]} ROWS FETCH NEXT ${range[1] + 1 - range[0]} ROWS ONLY`
+                sort[1]
+            }
+            OFFSET ${range[0]} ROWS FETCH NEXT ${range[1] + 1 - range[0]} ROWS ONLY`
         );
 
+
+        const recordsWithSeparator = result.recordset.map(record => {
+            if (record.id > 30) {
+                // Ajouter le séparateur avant d'afficher l'ID 32
+               
+                record.designation = `Designation des factures 2024-${record.designation}`;
+            }else {
+                record.designation = `Ancien designation-${record.designation}`;
+            }
+            return record;
+        });
+
+        // const recordsWithSeparator = result.recordset.map(record => {
+        //     if (record.id > 30 && record.id < 31) {
+        //         // Ajouter deux lignes vides avec des valeurs spécifiques avant d'afficher l'ID 32
+        //         const emptycodeDesignation = { codeDesignation: '2024' }; 
+        //         const emptyDesignation = { designation: 'fin des designations 2023' }; 
+        //         return [emptycodeDesignation, emptyDesignation, record];
+        //     }
+        //     return record;
+        // }).flat(); 
+        
+      
         console.log(req.count);
         res.set(
             "Content-Range",
-            `fournisseurs ${range[0]}-${range[1] + 1 - range[0]}/${req.count}`
+            `desiganation ${range[0]}-${range[1] + 1 - range[0]}/${req.count}`
         );
-        res.json(result.recordset);
+        res.json(recordsWithSeparator);
+        console.log(recordsWithSeparator)
     } catch (error) {
         res.status(500);
         res.send(error.message);
