537a2e187ac70ad89f5b0eb182df5522e2bf9248
youssefihrai <49070572+youssefihrai@users.noreply.github.com>
2024-08-09 09:47:05 +0100

    Change Ip Adresse & password

--- database/connection.js
+++ database/connection.js
@@ -1,39 +1,39 @@
 const sql = require("mssql");
-const sql2 = require("mssql/msnodesqlv8");
-const dbSettings2 = {
+// const sql2 = require("mssql/msnodesqlv8");
+// const dbSettings2 = {
+//   database: "ATNER_DW",
+//   server: "YIHRAI-JJ53ODN\\LOCALHOST_TEST",
+//   driver: "msnodesqlv8",
+//   pool: {
+//     max: 10,
+//     min: 0,
+//     idleTimeoutMillis: 30000,
+//   },
+//   options: {
+//     trustedConnection: true,
+//     requestTimeout: 2000000, // Augmenter le délai d'attente
+//   },
+// };
+
+const dbSettings = {
+  user: "saisie.erp",
+  password: "NouveauM0tDeP@ss!",
+  server: "10.111.1.68",
   database: "ATNER_DW",
-  server: "YIHRAI-JJ53ODN\\LOCALHOST_TEST",
-  driver: "msnodesqlv8",
   pool: {
     max: 10,
     min: 0,
     idleTimeoutMillis: 30000,
   },
   options: {
-    trustedConnection: true,
-    requestTimeout: 2000000, // Augmenter le délai d'attente
+    encrypt: false,
+    trustServerCertificate: true,
   },
 };
 
-// const dbSettings = {
-//   user: "saisie.erp",
-//   password: "Sage123+",
-//   server: "192.168.1.202",
-//  database: "ATNER_DW",
-//   pool: {
-//     max: 10,
-//     min: 0,
-//     idleTimeoutMillis: 30000
-//   },
-//   options: {
-//     encrypt: false,
-//     trustServerCertificate: true
-//   }
-// };
-
 exports.getConnection = async () => {
   try {
-    const pool = await sql.connect(dbSettings2);
+    const pool = await sql.connect(dbSettings);
     return pool;
   } catch (error) {
     console.error(error);
@@ -41,5 +41,5 @@ exports.getConnection = async () => {
 };
 
 exports.getSql = () => {
-  return sql2;
+  return sql;
 };
