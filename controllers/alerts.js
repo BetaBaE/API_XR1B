30a47e302fab43783103629d63f82ed13cd98f92
Sohaib Arbi BAKCHA <sohaibbakcha@gmail.com>
2024-10-15 16:32:03 +0100

    Add Grid Of facture ayant Fn Sage

--- controllers/alerts.js
+++ controllers/alerts.js
@@ -131,3 +131,56 @@ exports.getRasTvaCount = async (req, res, next) => {
     res.send(error.message);
   }
 };
+
+exports.getFactureAyantFNSage = async (req, res) => {
+  try {
+    let range = req.query.range || "[0,9]";
+    let sort = req.query.sort || '["id" , "ASC"]';
+    let filter = req.query.filter || "{}";
+    range = JSON.parse(range);
+    sort = JSON.parse(sort);
+    filter = JSON.parse(filter);
+
+    let queryFilter = "";
+
+    if (filter.nom) {
+      queryFilter += ` and upper(fou.nom) like(upper('%${filter.nom}%'))`;
+    }
+
+    const pool = await getConnection();
+    console.log(`${Alerts.FactureAyantFN} Order by ${sort[0]} ${sort[1]}`);
+
+    const result = await pool.request().query(`${
+      Alerts.FactureAyantFN
+    } ${queryFilter} Order by ${sort[0]} ${sort[1]}
+        OFFSET ${range[0]} ROWS FETCH NEXT ${
+      range[1] + 1 - range[0]
+    } ROWS ONLY`);
+
+    res.set(
+      "Content-Range",
+      `faayantfn ${range[0]}-${range[1] + 1 - range[0]}/${req.count}`
+    );
+
+    res.json(result.recordset);
+  } catch (error) {
+    res.send(error.message);
+    res.status(500);
+  }
+};
+
+exports.getFactureAyantFNSageCount = async (req, res, next) => {
+  try {
+    const pool = await getConnection();
+    const result = await pool.request().query(Alerts.FactureAyantFNCount);
+
+    req.count = result.recordset[0].count;
+    console.log(req.count);
+    // res.json({ count: res.conut });
+    next();
+  } catch (error) {
+    res.status(500);
+    console.log(error.message);
+    res.send(error.message);
+  }
+};
