fd0167758b4ef41208a98c7bd8ca59230ff9eb86
Sohaib Arbi BAKCHA <sohaibbakcha@gmail.com>
2025-04-08 10:44:39 +0100

    modify trhe eachence chart logic

--- database/charts/overdueInvoicesQuery.js
+++ database/charts/overdueInvoicesQuery.js
@@ -1,5 +1,6 @@
 exports.overdueInvoicesQuery = {
   query: `
+  /*
 WITH antecedent AS (
     SELECT 
         SUM(CASE WHEN Etat = 'Saisie' THEN TTC - AcompteVal ELSE 0 END) AS montantSaisie,
@@ -57,6 +58,62 @@ SELECT * FROM (
     FROM antecedent a, unMois u, deuxMois d
 ) t
 ORDER BY montantSaisie DESC, montantEnCours DESC;
+--==========================================================================
+--==========================================================================
+--==========================================================================
+*/
 
+with FaExtand as (
+	
+	select idFacture , sum(Montant) as rsMontant from DAF_RestitAvance rs
+	where idFacture is not null and etat <> 'Annuler'
+	group by idFacture
+ 
+),
+    echue as 
+(
+select 
+format(getdate(),'yyyy-MM') Mech,
+format(getdate(),'yyyy-MM') name,
+SUM(CASE WHEN Etat = 'Saisie' THEN TTC - COALESCE (fe.rsMontant,0) ELSE 0 END) AS montantSaisie,
+SUM(CASE WHEN Etat = 'En Cours' THEN TTC - COALESCE (fe.rsMontant,0) ELSE 0 END) AS montantEnCours
+--sum((fs.ttc - COALESCE (fe.rsMontant,0) )) netapayer/*,sum((fs.ttc - fs.AcompteVal )) netapayer1*/  
+from DAF_FactureSaisie fs
+left join DAF_FOURNISSEURS fr on fs.idfournisseur=fr.id
+left join DAF_EcheanceFournisseur e on fs.idfournisseur=e.idFournisseur
+left join  FaExtand fe on fs.id = fe.idFacture
+where 
+fs.etat in ('Saisie','En Cours') and 
+format(DATEADD(day,iif(e.EcheanceJR is null,60,e.EcheanceJR),fs.DateFacture),'yyyy-MM')<=format(getdate(),'yyyy-MM')
+)
+,
+echeavenir as (
+select format(DATEADD(day,iif(e.EcheanceJR is null,60,e.EcheanceJR),fs.DateFacture),'yyyy-MM') mech,
+format(DATEADD(day,iif(e.EcheanceJR is null,60,e.EcheanceJR),fs.DateFacture),'yyyy-MM') name,
+SUM(CASE WHEN Etat = 'Saisie' THEN TTC - COALESCE (fe.rsMontant,0) ELSE 0 END) AS montantSaisie,
+SUM(CASE WHEN Etat = 'En Cours' THEN TTC - COALESCE (fe.rsMontant,0) ELSE 0 END) AS montantEnCours
+--sum((fs.ttc - COALESCE (fe.rsMontant,0) )) netapayer/*,sum((fs.ttc - fs.AcompteVal )) netapayer1*/  
+from DAF_FactureSaisie fs
+left join DAF_FOURNISSEURS fr on fs.idfournisseur=fr.id
+left join DAF_EcheanceFournisseur e on fs.idfournisseur=e.idFournisseur
+left join  FaExtand fe on fs.id = fe.idFacture
+where fs.etat in ('Saisie','En Cours') and format(DATEADD(day,iif(e.EcheanceJR is null,60,e.EcheanceJR),fs.DateFacture),'yyyy-MM')>format(getdate(),'yyyy-MM')
+
+group by format(DATEADD(day,iif(e.EcheanceJR is null,60,e.EcheanceJR),fs.DateFacture),'yyyy-MM')
+)
+
+select 'Total' as id,
+ 'Total' as name,
+ sum(montantSaisie) as montantSaisie
+, sum (montantEnCours) as  montantEnCours
+from (
+select * from echue
+union all 
+select * from echeavenir
+) t
+union all 
+select * from echue
+union all 
+select * from echeavenir
 `,
 };
