3acc81f18ded66ad13d9d37aa6957658917540f6
Sohaib Bakcha <sohaibbakcha@gmail.com>
2026-03-11 12:24:16 +0000

    rollback

--- controllers/Attestation.js
+++ controllers/Attestation.js
@@ -8,7 +8,7 @@ const {
 exports.GetAttestation = async (req, res) => {
   try {
     let range = req.query.range || "[0,9]";
-    let sort = req.query.sort || '["priorite" , "ASC"]';
+    let sort = req.query.sort || '["nom" , "ASC"]';
     let filter = req.query.filter || "{}";
     range = JSON.parse(range);
     sort = JSON.parse(sort);
@@ -24,10 +24,7 @@ exports.GetAttestation = async (req, res) => {
     if (filter.statut) {
       switch (filter.statut) {
         case "pas_attestation":
-          queryFilter += ` and att.[dateExpiration] IS NULL and fou.[datecreation] < DATEADD(MONTH, -5, GETDATE())`;
-          break;
-        case "nouveau_fournisseur":
-          queryFilter += ` and att.[dateExpiration] IS NULL and fou.[datecreation] >= DATEADD(MONTH, -5, GETDATE())`;
+          queryFilter += ` and att.[dateExpiration] IS NULL`;
           break;
         case "expire":
           queryFilter += ` and att.[dateExpiration] IS NOT NULL and DATEDIFF(DAY, GETDATE(), att.[dateExpiration]) < 0`;
@@ -64,7 +61,6 @@ exports.GetAttestation = async (req, res) => {
     res.send(error.message);
   }
 };
-
 /* 
  lister le nombre des attestations 
  cette fonction  nous aide dans la pagination
